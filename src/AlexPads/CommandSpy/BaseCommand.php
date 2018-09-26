7e1602ac0726cf44b5c46421ecc888200319362c
AlexPads <37447608+AlexPads@users.noreply.github.com>
2018-09-26 08:44:31 -0400

    Add files via upload

---
+++ src/AlexPads/CommandSpy/BaseCommand.php
@@ -0,0 +1,16 @@
+<?php
+namespace AlexPads\CommandSpy;
+use pocketmine\command\Command;
+use pocketmine\command\PluginIdentifiableCommand;
+use SocialSpy\Main;
+abstract class BaseCommand extends Command implements PluginIdentifiableCommand {
+    public $prefix = "§8[§aSocial Spy§8]§6 ";
+	private $plugin;
+	public function __construct(Main $plugin, string $name, string $desc = "", string $usage, array $aliases = []){
+		parent::__construct($name, $desc, $usage, $aliases);
+		$this->plugin = $plugin;	
+       }
+	public final function getPlugin(): Command{
+		return $this->plugin;
+	}
+}
