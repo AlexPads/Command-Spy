bcc0ac0f74d05f5cbc7b2244d447bf127cbcda00
imwood04 <41460038+imwood04@users.noreply.github.com>
2018-09-26 21:18:08 -0400

    Test

--- src/CommandSpy/TinyPixelDevz/AlexPads/BaseCommand.php
+++ src/CommandSpy/TinyPixelDevz/AlexPads/BaseCommand.php
@@ -7,7 +7,7 @@ use pocketmine\command\PluginIdentifiableCommand;
 use CommandSpy\Main;
 
 abstract class BaseCommand extends Command implements PluginIdentifiableCommand {
-    public $prefix = "§8[§aVM§8]§6 ";
+    public $prefix = "§8[§aVM§8]§6";
 	private $plugin;
 	public function __construct(Main $plugin, string $name, string $desc = "", string $usage, array $aliases = []){
 		parent::__construct($name, $desc, $usage, $aliases);
